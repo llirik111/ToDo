42cba9e58de15ad7d1f19a40d742c38d794da99b
fillkey <vasaparta@mail.ru>
2025-10-03 11:41:59 +1000

    Добавил кнопку задачавыполнена,  создал класс под зачу

--- ToDo/MainWindow.xaml.cs
+++ ToDo/MainWindow.xaml.cs
@@ -1,24 +1,7 @@
-﻿using System.Text;
+﻿using System;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Numerics;
+using System.Runtime.CompilerServices;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
-
-namespace ToDo
-{
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
-    public partial class MainWindow : Window
-    {
-        public MainWindow()
-        {
-            InitializeComponent();
-        }
-    }
-}
---
+++ ToDo/mamka/mamka2.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ToDo.mamka
+{
+    public class mamka2
+    {
+        public string Zagolovok {  get; set; }
+        public int Indificator { get; set; }
+        public bool Proverkagotovnosti { get; set; }
+        public DateTime Date {  get; set; }
+
+    }
+}
